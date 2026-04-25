97a313e21b972af5266a433727f3f0f59162bc08
Rohail Taimour <rohail.taimour@gmail.com>
2026-04-25 21:34:26 +0200

    Simplify the approach

--- plugin/init.lua
+++ plugin/init.lua
@@ -23,18 +23,14 @@ local function switch_to_alternate_workspace_action()
   end)
 end
 
-local function make_switcher(window, pane)
-  return function(name, spawn)
-    perform_tracked_switch(window, pane, name, spawn)
-  end
-end
-
 function M.switch_workspace(callback)
-  local function handle_selector(window, pane, path, label)
-    local do_switch =  make_switcher(window, pane)
-    callback(do_switch, path, label)
-  end
-  return wezterm.action_callback(handle_selector)
+  return wezterm.action_callback(function(window, pane, path, label)
+    if not path then return end
+    local function switch(name, spawn)
+      perform_tracked_switch(window, pane, name, spawn)
+    end
+    callback(switch, path, label)
+  end)
 end
 
 function M.apply_to_config(config)
